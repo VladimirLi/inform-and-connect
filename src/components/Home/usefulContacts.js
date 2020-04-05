3c2fdc5de9f9091451216c90ea4bacc2851ebb80
Vladimir <vladde1991@gmail.com>
2020-04-05 16:18:02 +0200

    Final commit

--- src/components/Home/usefulContacts.js
+++ src/components/Home/usefulContacts.js
@@ -13,12 +13,47 @@ class UsefulContacts extends Component {
               this.props.currentLanguage.code
             )}
           </h3>
-          {this.props.contacts.map((contact) => (
+          <h5>Medical advice in other languages</h5>
+          <p>
+            When you need medical advice in other languages, please call:
+            <ul>
+              <li>
+                English: 1178 <br />
+                all week, around the clock
+              </li>
+              <li>
+                Finnish: 1177 <br />
+                weekdays 8–12 am
+              </li>
+              <li>
+                Arabic: 08-123 130 80 <br />
+                all week 8 am–22 pm
+              </li>
+              <li>
+                Somali: 08-123 130 90 <br />
+                weekdays 8 am–17 pm
+              </li>
+              <li>
+                Persian: 08-123130 87 <br />
+                weekdays 8 am–17 pm
+              </li>
+            </ul>
+          </p>
+          <h5>General questions about coronavirus</h5>
+          <p>
+            Phone service for questions concerning covid-19 for you who speak{" "}
+            <b>Arabic</b>, <b>Somali</b>, <b>Persian</b>,{" "}
+            <b>Tigrinya/Amarinja</b> and <b>Russian</b>. <br /> Phone: 08-123
+            680 00.
+            <br />
+            Opening hours weekdays, 9-12 am and 13-15 pm.
+          </p>
+          {/* {this.props.contacts.map((contact) => (
             <div key={contact.id} className="mb-5">
               <h5>{contact.number}</h5>
               {contact.text}
             </div>
-          ))}
+          ))} */}
         </div>
       );
     } else {
